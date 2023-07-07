1c2d7997dfbd306f98d3f884b63799bdb5f00894
AngelWinnie <angel04110901@gmail.com>
2023-07-07 09:20:25 +0800

    reflash

---
+++ js/home.js
@@ -0,0 +1,90 @@
+function toyko_card1() {
+  window.open('tokyo_card1.html', '_self');
+} 
+
+function toyko_card2() {
+  window.open('tokyo_card2.html', '_self');
+} 
+
+function toyko_card3() {
+  window.open('tokyo_card3.html', '_self');
+}
+
+function toyko_card4() {
+  window.open('tokyo_card4.html', '_self');
+}
+// 大阪卡片
+
+function osaka_card1() {
+  window.open('osaka_card1.html', '_self');
+} 
+ 
+function osaka_card2() {
+  window.open('osaka_card2.html', '_self');
+} 
+
+function osaka_card3() {
+  window.open('osaka_card3.html', '_self');
+}
+
+function osaka_card4() {
+  window.open('osaka_card4.html', '_self');
+}
+
+// 九州卡片
+function kyushu_card1() {
+  window.open('kyushu_card1.html', '_self');
+} 
+ 
+function kyushu_card2() {
+  window.open('kyushu_card2.html', '_self');
+} 
+
+function kyushu_card3() {
+  window.open('kyushu_card3.html', '_self');
+}
+
+function kyushu_card4() {
+  window.open('kyushu_card4.html', '_self');
+}
+
+// 廣島卡片
+function hiroshima_card1() {
+  window.open('hiroshima_card1.html', '_self');
+} 
+ 
+function hiroshima_card2() {
+  window.open('hiroshima_card2.html', '_self');
+} 
+
+function hiroshima_card3() {
+  window.open('hiroshima_card3.html', '_self');
+}
+
+function hiroshima_card4() {
+  window.open('hiroshima_card4.html', '_self');
+}
+
+
+// 首頁換頁面連結
+function tokyo1() {
+  window.open('home.html','_self');
+}
+
+function osaka() {
+  window.open('osaka.html','_self');
+}
+
+function kyushu() {
+  window.open('kyushu.html','_self');
+}
+
+function hiroshima() {
+  window.open('hiroshima.html','_self');
+}
+// var card = document.querySelector()
+// var card = document.querySelector('.card1');
+// card.addEventListener('click', function () {
+//   // window.open('https://www.google.com', '_blank');
+//   console.log('card1');
+// });
